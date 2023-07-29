eb352dd9694bf3694a64173f9df300cdd8e2c35e
Sauter <gabriel.andrade.sauter@gmail.com>
2023-07-29 13:39:12 -0300

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,16 @@ import 'package:progfile/app/views/register_view.dart';
 
 import 'app/views/login_view.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
