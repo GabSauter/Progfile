491b98c787d27e3adb514f1b8ac67fd75326673c
Sauter <gabriel.andrade.sauter@gmail.com>
2023-09-09 17:51:57 -0300

    Changed the ImageDisplay to take a photo

--- lib/app/views/components/image_take.dart
+++ lib/app/views/components/image_take.dart
@@ -15,9 +15,16 @@ class _ImageDisplayState extends State<ImageDisplay> {
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
+      backgroundColor: Theme.of(context).primaryColor,
+      foregroundColor: const Color.fromARGB(255, 255, 255, 255),
       radius: 70.0,
       backgroundImage: widget.image != null ? FileImage(widget.image!) : null,
-      child: widget.image == null ? const Icon(Icons.camera_alt) : null,
+      child: widget.image == null
+          ? const Icon(
+              Icons.camera_alt,
+              size: 32,
+            )
+          : null,
     );
   }
 }
