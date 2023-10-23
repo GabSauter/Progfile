fba54acd37a5c19fca132e51a5348b99b04c47c1
Ninjaalpha01 <gabriel_leomartins@hotmail.com>
2023-10-23 17:00:43 -0300

    changing name of homeview

--- lib/app/repositories/curriculum_reposiotory.dart
+++ lib/app/repositories/curriculum_reposiotory.dart
@@ -45,6 +45,8 @@ class CurriculumRepository extends ChangeNotifier {
     curriculum.gitProjects = gitProjects;
     curriculum.languages = languages;
 
+    notifyListeners();
+
     return curriculum;
   }
 
