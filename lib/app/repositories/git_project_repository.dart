e613bf0f436ec98e30d8e51bb6fb2995904c4804
Gabriel Leonardo Martins <101340379+Ninjaalpha01@users.noreply.github.com>
2023-11-29 16:56:17 -0300

    up

--- lib/app/repositories/git_project_repository.dart
+++ lib/app/repositories/git_project_repository.dart
@@ -3,15 +3,20 @@ import 'dart:collection';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/git_project_model.dart';
 
 class GitProjectRepository extends ChangeNotifier {
   final _db = FirebaseFirestore.instance;
   final List<GitProjectModel> _projects = [];
+  final List<GitProjectModel> _projectsApi = [];
 
   UnmodifiableListView<GitProjectModel> get list =>
       UnmodifiableListView(_projects);
+
+  UnmodifiableListView<GitProjectModel> get listApi =>
+      UnmodifiableListView(_projectsApi);
 
   getProjects() async {
     _projects.clear();
@@ -26,6 +31,18 @@ class GitProjectRepository extends ChangeNotifier {
       GitProjectModel project = GitProjectModel.fromMap(doc.data());
       project.id = doc.id;
       _projects.add(project);
+    }
+
+    String username = "oi";
+    String url = "https://api.github.com/users/Ninjaalpha01/repos";
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      List<dynamic> repos = response.body as List;
+      print(repos[0]["name"] + 'bananou');
+    } else {
+      print(response.statusCode);
     }
 
     notifyListeners();
